a92b7382c39359020f1f72f47ba6098794ecbef2
abhishekkapoor9 <abhishekkpr9@gmail.com>
2017-06-02 08:56:53 +0530

    multiplication

--- addition.cs
+++ addition.cs
@@ -13,3 +13,10 @@ console.writeline('Enter Second Name');
 int b=console.readline();
 int c=a-b;
 console.writeline('Substraction:{0}'c);
+
+console.writeline('Enter first Name');
+int a=console.readline();
+console.writeline('Enter Second Name');
+int b=console.readline();
+int c=a*b;
+console.writeline('Multiplication:{0}'c);
