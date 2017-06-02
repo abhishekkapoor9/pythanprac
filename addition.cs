2a24710b5e8093a516deaceb6482c7c5e3a7404d
abhishekkapoor9 <abhishekkpr9@gmail.com>
2017-06-02 08:48:18 +0530

    Substraction

--- addition.cs
+++ addition.cs
@@ -3,3 +3,13 @@ int a=console.readline();
 console.writeline('Enter Second Name');
 int b=console.readline();
 int c=a+b;
+console.writeline('Addition:{0}'c);
+
+
+
+console.writeline('Enter first Name');
+int a=console.readline();
+console.writeline('Enter Second Name');
+int b=console.readline();
+int c=a-b;
+console.writeline('Substraction:{0}'c);
